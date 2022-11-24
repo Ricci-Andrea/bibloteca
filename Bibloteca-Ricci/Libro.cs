afe76247c6d8f372fa0bffd61b549c616d57da95
Andrea Ricci <ANDREA.RICCI@local.ittcesena.it>
2022-11-24 08:39:42 +0100

    implementazione della classe Libro

---
+++ Bibloteca-Ricci/Libro.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibloteca_Ricci
+{
+    internal class Libro
+    {
+        internal string Autore { get; set; }
+        internal string titolo { get; set; }
+        internal string AnnoDiPublicazione { get; set; }
+        internal string Editore { get; set; }
+        internal int numeroDiPagine { get; set; }
+
+        internal  string toString(Libro Libro) {
+
+            return $"L'autore {Libro.Autore}, Titolo: {Libro.titolo}, anno Di Publicazione: {Libro.AnnoDiPublicazione}, editore: {Libro.Editore}, numero Di Paggine: {Libro.numeroDiPagine} ";
+        
+        }
+
+        internal string readingTime(Libro Libro)
+        { 
+            int tempoDiLettura = 0;
+
+            if (Libro.numeroDiPagine < 100)
+            {
+                tempoDiLettura = 1;
+            }
+            else if (Libro.numeroDiPagine > 100 || Libro.numeroDiPagine < 200)
+            {
+                tempoDiLettura = 2;
+            }
+            else 
+            {
+                return "Il tempo Di lettura e superiore alle due ore";
+
+            }
+            return tempoDiLettura.ToString();
+        
+        }
+
+    }
+}
