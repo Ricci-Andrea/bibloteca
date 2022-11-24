fd7da41a2497035555d745c8776d5b2aa26b107e
Andrea Ricci <ANDREA.RICCI@local.ittcesena.it>
2022-11-24 08:55:51 +0100

    Sviluppo prima parte della classe bibloteca

---
+++ Bibloteca-Ricci/Bibloteca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bibloteca_Ricci
+{
+    internal class Bibloteca
+    {
+        internal string Nome { get; set; }
+        internal int orarioDiApertura { get; set; }
+        internal int orarioDiCiusura { get; set; }
+        List<Libro> ElenciLibri = new();
+
+     
+    }
+    
+    
+}
+
